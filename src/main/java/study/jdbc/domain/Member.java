25c790a2268a104bc0bc231c754789f321391004
qpakr38 <zczc0819@gmail.com>
2022-04-15 13:45:20 +0900

    feature : Member 객체 생성

---
+++ src/main/java/study/jdbc/domain/Member.java
@@ -0,0 +1,13 @@
+package study.jdbc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private String memberId;
+    private int money;
+}
