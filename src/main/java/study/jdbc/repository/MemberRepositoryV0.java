d56c4d72f416e19c1169085708ec542e51e9d468
qpakr38 <zczc0819@gmail.com>
2022-04-15 13:46:03 +0900

    feature : JDBC를 활용한 member 저장 기능 구현

---
+++ src/main/java/study/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,61 @@
+package study.jdbc.repository;
+
+/*
+ * JDBC - DriverManager 사용
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import study.jdbc.connection.DBConnectionUtil;
+import study.jdbc.domain.Member;
+
+import java.sql.*;
+
+@Slf4j
+public class MemberRepositoryV0 {
+    public Member save(Member member) throws SQLException {
+        String sql = "INSERT INTO MEMBER(MEMBER_ID, MONEY) VALUES(?, ?)";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
