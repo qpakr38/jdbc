4649fe944292642120e80e0e867681db4ab9668c
qpakr38 <zczc0819@gmail.com>
2022-04-15 14:11:07 +0900

    TEST : member 조회 기능 테스트

--- src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,28 @@
 package study.jdbc.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import study.jdbc.domain.Member;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberA", 10000);
+        Member member = new Member("memberB", 10000);
         repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}",findMember);
+        log.info("member={}",member);
+        assertThat(findMember).isEqualTo(member);
+
     }
 
 }
