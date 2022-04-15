e5504e61ac9f10027d2768a67a2857b2f64c13a7
qpakr38 <zczc0819@gmail.com>
2022-04-15 12:52:53 +0900

    FEAT : DB연결을 위한 상수 선언

---
+++ src/main/java/study/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package study.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/./db/springDB";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "sa";
+}
