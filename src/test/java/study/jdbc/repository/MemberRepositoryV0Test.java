eb3283ab045ad9b86b5af089fd777ea201e84e25
qpakr38 <zczc0819@gmail.com>
2022-04-16 14:22:38 +0900

    TEST : member money 삭제 기능 테스트

--- src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/study/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import study.jdbc.domain.Member;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,21 +17,35 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberC", 10000);
+        Member member = new Member("memberD", 10000);
         repository.save(member);
 
         //findById
         Member findMember = repository.findById(member.getMemberId());
-        log.info("findMember={}",findMember);
-        log.info("member={}",member);
+        log.info("findMember={}", findMember);
+        log.info("member={}", member);
         assertThat(findMember).isEqualTo(member);
 
         //update : money
-        repository.update(member.getMemberId(),20000);
+        repository.update(member.getMemberId(), 20000);
         Member updatedMember = repository.findById(member.getMemberId());
 
         assertThat(updatedMember.getMoney()).isEqualTo(20000);
 
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
     }
+
+    @Test
+    void test(){
+        assertThatThrownBy(()->exceptionTest()).isInstanceOf(RuntimeException.class);
+    }
+    void exceptionTest() throws Exception {
+        throw new Exception();
+    }
+
 
 }
