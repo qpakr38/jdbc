9c95fb5cd993ffd87790ac968316d26507e70af4
qpakr38 <zczc0819@gmail.com>
2022-04-18 14:20:15 +0900

    style : 띄어쓰기 수정

--- src/test/java/study/jdbc/connection/ConnectionTest.java
+++ src/test/java/study/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package study.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -19,8 +20,8 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        log.info("connection = {}, class = {}",con1,con1.getClass());
-        log.info("connection = {}, class = {}",con2,con2.getClass());
+        log.info("connection = {}, class = {}", con1, con1.getClass());
+        log.info("connection = {}, class = {}", con2, con2.getClass());
     }
 
     @Test
@@ -29,10 +30,25 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
-        log.info("connection = {}, class = {}",con1,con1.getClass());
-        log.info("connection = {}, class = {}",con2,con2.getClass());
+
+        log.info("connection = {}, class = {}", con1, con1.getClass());
+        log.info("connection = {}, class = {}", con2, con2.getClass());
+
     }
 }
